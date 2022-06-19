2408f2038134b3af2e5f1d7dd706a15d37705723
Sandeep Yempaty <87543070+silentsword03@users.noreply.github.com>
2022-06-19 21:56:57 +0200

    Update dashboard.py
    Installed dependencies

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
+#import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 
